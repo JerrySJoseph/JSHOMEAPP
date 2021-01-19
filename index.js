da0b564cfda2b6f95864049d2174f2e6bc132748
Jerry S Joseph <jerin.sebastian153@gmail.com>
2021-01-19 21:14:47 +0530

    second commit

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   response.end('Hello from JS Industries\n');
 }).listen(PORT);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at PORT:'+PORT);
